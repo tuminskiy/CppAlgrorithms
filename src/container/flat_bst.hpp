31c1aa1b740060131ffbe69f1fa8839f552caf0d
tuminskiy <unknowuzvername@gmail.com>
2020-11-11 22:10:14 +0300

    Add functions for iterate: pre-order (NLR), in-order (LNR), post-order (LRN)

--- src/container/flat_bst.hpp
+++ src/container/flat_bst.hpp
@@ -14,6 +14,10 @@ public:
   void insert(const T& value);
   void insert(const std::initializer_list<T>& il);
 
+  template <class Func> void nlr_iterate(Func f);
+  template <class Func> void lnr_iterate(Func f);
+  template <class Func> void lrn_iterate(Func f);
+
 private:
   std::vector<std::optional<T>> data_;
 
@@ -21,7 +25,13 @@ private:
   size_t right(size_t parent) const;
   size_t parent(size_t index) const;
 
+  bool is_valid(size_t index) const;
+
   void insert(size_t index, const T& value);
+
+  template <class Func> void nlr_iterate(size_t index, Func f);
+  template <class Func> void lnr_iterate(size_t index, Func f);
+  template <class Func> void lrn_iterate(size_t index, Func f);
 };
 
 
@@ -36,6 +46,16 @@ void FlatBst<T>::insert(const std::initializer_list<T>& il)
 }
 
 
+template <class T> template <class Func>
+void FlatBst<T>::nlr_iterate(Func f) { return nlr_iterate(0, f); }
+
+template <class T> template <class Func>
+void FlatBst<T>::lnr_iterate(Func f) { return lnr_iterate(0, f); }
+
+template <class T> template <class Func>
+void FlatBst<T>::lrn_iterate(Func f) { return lrn_iterate(0, f); }
+
+
 template <class T>
 size_t FlatBst<T>::left(size_t parent) const { return parent * 2 + 1; }
 
@@ -44,6 +64,13 @@ size_t FlatBst<T>::right(size_t parent) const { return parent * 2 + 2; }
 
 template <class T>
 size_t FlatBst<T>::parent(size_t index) const { return (index - 1) / 2; }
+
+
+template <class T>
+bool FlatBst<T>::is_valid(size_t index) const
+{
+  return index < data_.size() && data_[index];
+}
 
 
 template <class T>
@@ -58,6 +85,41 @@ void FlatBst<T>::insert(size_t index, const T& value)
     insert(left(index), value);
   else if (data_[index] < value)
     insert(right(index), value);
+}
+
+
+template <class T> template <class Func>
+void FlatBst<T>::nlr_iterate(size_t index, Func f)
+{
+  if (!is_valid(index))
+    return;
+
+  f(data_[index].value());
+
+  nlr_iterate(left(index), f);
+  nlr_iterate(right(index), f);
+}
+
+template <class T> template <class Func>
+void FlatBst<T>::lnr_iterate(size_t index, Func f)
+{
+  if (!is_valid(index))
+    return;
+
+  nlr_iterate(left(index), f);
+  f(data_[index].value());
+  nlr_iterate(right(index), f);
+}
+
+template <class T> template <class Func>
+void FlatBst<T>::lrn_iterate(size_t index, Func f)
+{
+  if (!is_valid(index))
+    return;
+
+  nlr_iterate(left(index), f);
+  nlr_iterate(right(index), f);
+  f(data_[index].value());
 }
 
 } // namespace Container
